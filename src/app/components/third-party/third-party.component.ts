bfa06871fe14b36ab24abce391fb515e98da8800
lawcorredor <lauram.co.sa@gmail.com>
2018-04-05 20:21:13 -0500

    Adds third partys table

--- src/app/components/third-party/third-party.component.ts
+++ src/app/components/third-party/third-party.component.ts
@@ -12,7 +12,9 @@ export class ThirdPartyComponent implements OnInit {
 
   public third_party: ThirdParty;
   public editable = false;
-
+  public third_partys: ThirdParty[];
+  public key;
+  
   constructor(private thirdPartyService: ThirdPartyService) {
     this.third_party = new ThirdParty();
   }
@@ -23,6 +25,17 @@ export class ThirdPartyComponent implements OnInit {
 
   public set_type(type: number) {
     this.third_party.third_type = type;
+  }
+
+  public find(search: string) {
+    if(search == "ALL"){
+      this.third_partys = this.thirdPartyService.get_third_party();
+    }
+    this.key = search;
+  }
+
+  public view(third: ThirdParty){
+    this.third_party.third_type = third.third_type;
   }
 
 }
