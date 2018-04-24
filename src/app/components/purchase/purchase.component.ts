ba609bf561f507eacd259ad00298e3e217b0053d
Daniel Esteban Novoa Reyes <dnovoare@everis.com>
2018-04-24 13:50:20 -0500

    Adds the first purchase component version

--- src/app/components/purchase/purchase.component.ts
+++ src/app/components/purchase/purchase.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { Purchase } from './class/purchase';
 import { PurchaseService } from './services/purchase.service';
+import { NgbModal, ModalDismissReasons, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
+import { Purchase } from './class/purchase';
+import { PurchaseDetail } from './class/purchase-detail';
+import { Observable } from 'rxjs/observable';
 
 @Component({
   selector: 'app-purchase',
@@ -12,13 +15,50 @@ export class PurchaseComponent implements OnInit {
 
   private purchase: Purchase;
   private openPurchases;
+  private purchase_details: Observable<PurchaseDetail[]>;
+  private purchase_detail: PurchaseDetail;
 
-  constructor(private purchaseService: PurchaseService) {
+  private detailModalRef: NgbModalRef;
+
+  private products;
+  private presentations;
+
+  constructor(private purchaseService: PurchaseService,
+    private modalService: NgbModal) {
     this.purchase = new Purchase();
   }
 
   ngOnInit() {
     this.openPurchases = this.purchaseService.get_open_purchases();
+    this.products = this.purchaseService.get_domain('PRODUCTO');
+    this.presentations = this.purchaseService.get_domain('PRESENTACION');
   }
 
+  open_modal(content, purchase: Purchase) {
+    this.modalService.open(content, { size: 'lg' });
+    this.purchase = purchase;
+    this.get_details();
+  }
+
+  get_details() {
+    this.purchase_details = this.purchaseService.get_details(this.purchase.id);
+  }
+
+  add(content, detail: PurchaseDetail) {
+    this.detailModalRef = this.modalService.open(content, { size: 'sm' });
+    this.purchase_detail = detail;
+  }
+
+  saveDetail() {
+    this.purchase_detail.purchase_id = this.purchase.id;
+    this.purchase_detail.total_cost = this.purchase_detail.unitary_cost * this.purchase_detail.amount;
+    this.purchaseService.save_detail(this.purchase_detail).subscribe(res => {
+      if (res.error === 0) {
+        this.detailModalRef.close('Close click');
+        this.get_details();
+      } else {
+        alert('Error');
+      }
+    });
+  }
 }
--- src/app/components/purchase/services/purchase.service.ts
+++ src/app/components/purchase/services/purchase.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpRequest } from '@angular/common/http';
 import { Purchase } from '../class/purchase';
+import { PurchaseDetail } from '../class/purchase-detail';
 import { Observable } from 'rxjs/observable';
 import 'rxjs/add/operator/map';
 
-const WS_URL = '//localhost:2900/';
+const WS_URL = 'http://api.frucun.com:2900/';
 const METHOD = 'purchase';
 
 @Injectable()
@@ -14,8 +15,13 @@ export class PurchaseService {
   private httpOptions = {
       headers: new HttpHeaders({
           // 'Content-Type': 'application/json',
-          'Content-Type': 'application/x-www-form-urlencoded',
-          'userKey': this.userKey
+          'Content-Type': 'application/json; charset=utf-8',
+          'Data-Type': 'json',
+          'userKey': this.userKey,
+          'method': 'POST',
+          'Access-Control-Allow-Headers': 'Content-Type',
+          'Access-Control-Allow-Methods': 'POST',
+          'Access-Control-Allow-Origin': '*'
       })
   };
 
@@ -24,22 +30,64 @@ export class PurchaseService {
   public get_open_purchases(): Observable<Purchase[]> {
     const apiUrl = `${WS_URL}${METHOD}/open`;
     return this.http.get(apiUrl)
-            .map(res => {
-              if (res === null || res === undefined) {
-                return null;
-              }
-              const results = (res as any[]).map( item => {
-                return new Purchase(
-                  item.id,
-                  item.third_party_id,
-                  item.date,
-                  item.sub_cost,
-                  item.cost,
-                  item.state
-                );
-              });
-              return results;
-            });
+      .map(res => {
+        if (res === null || res === undefined) {
+          return null;
+        }
+        const results = (res as any[]).map( item => {
+          return new Purchase(
+            item.id,
+            item.third_party_id,
+            item.date,
+            item.sub_cost,
+            item.total_cost,
+            item.state
+          );
+        });
+        return results;
+      });
+  }
+
+  public get_details(id: number): Observable<PurchaseDetail[]> {
+    const apiUrl = `${WS_URL}${METHOD}/detail?purchase_id=${id}`;
+    return this.http.get(apiUrl)
+      .map(res => {
+        if (res === null || res === undefined) {
+          return null;
+        }
+        const result = (res as any[]).map(item => {
+          return new PurchaseDetail(
+            item.id,
+            item.purchase_id,
+            item.product_id,
+            item.presentation_id,
+            item.amount,
+            item.unitary_cost,
+            item.total_cost
+          );
+        });
+        return result;
+      });
+  }
+
+  public get_domain(table: string): Observable<any[]> {
+    const apiUrl = `${WS_URL}domain?table=${table}`;
+    return this.http.get(apiUrl)
+    .map(res => {
+      if (res === null || res === undefined) {
+        return null;
+      }
+      const result = (res as any[]).map(item => {
+        return {id: btoa(item[0]), name: item[1]};
+      });
+      return result;
+    });
+  }
+
+  public save_detail(purchase_detail: PurchaseDetail) {
+    const apiUrl = `${WS_URL}${METHOD}/detail`;
+    console.log(purchase_detail);
+    return this.http.post<any>(apiUrl, purchase_detail);
   }
 
 }
